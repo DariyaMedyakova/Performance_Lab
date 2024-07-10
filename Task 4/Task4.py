285ca9f02ef7e1eacceb0e3f9adcb2eb830dc429
DariyaMedyakova <43128640+DariyaMedyakova@users.noreply.github.com>
2024-07-10 16:48:11 +0400

    Добавлена папка с решением Задачи 4

---
+++ Task 4/Task4.py
@@ -0,0 +1,14 @@
+with open('Values.txt') as f:
+    data = f.read().splitlines()
+f.close()
+
+num = []
+for el in data:
+    num.append(int(el))
+num.sort()
+
+countStep = 0
+LenNum = len(num)
+for i in range(0, LenNum // 2):
+    countStep = countStep + (num[LenNum - i - 1] - num[i])
+print(countStep)
